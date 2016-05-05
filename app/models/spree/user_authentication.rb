faacaa04baa0e48edb7f2f08a77ab3d413dd1f10
preety-singal <preetysingal@poplify.com>
2016-05-05 11:57:25 +0530

    first push

---
+++ app/models/spree/user_authentication.rb
@@ -0,0 +1,5 @@
+class Spree::UserAuthentication < ActiveRecord::Base
+  belongs_to :user
+
+  
+end
---
+++ app/models/spree/user_decorator.rb
@@ -0,0 +1,16 @@
+Spree.user_class.class_eval do
+  has_many :user_authentications, dependent: :destroy
+
+  devise :omniauthable
+
+  def apply_omniauth(omniauth)
+    if %w(facebook google_oauth2).include? omniauth['provider']
+      self.email = omniauth['info']['email'] if email.blank?
+    end
+    user_authentications.build(provider: omniauth['provider'], uid: omniauth['uid'])
+  end
+
+  def password_required?
+    (user_authentications.empty? || !password.blank?) && super
+  end
+end
